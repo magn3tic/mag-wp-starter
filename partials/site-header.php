fe5fd38b8a46994fe81b97148db5b03893127b87
Colin McGraw <colinmcgraw@me.com>
2023-09-03 09:39:50 -0700

    header partial example

---
+++ partials/site-header.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<header class="gc-header">
+  <div class="gc-inner">
+
+    <div class="gc-logo">
+      <svg xmlns="http://www.w3.org/2000/svg" width="305" height="72" viewBox="0 0 305 72" fill="none">
+      <path d="M304.016 0H0V71.581H304.016V0Z" fill="black"/>
+    </svg>
+    </div>
+  </div>
+</header>
